53d751546c4bb7cdf6519fccc073e731d2839760
ammarnasr <ammarnasraza@gmail.com>
2020-06-19 21:50:21 +0200

    Update inception_score.py

--- code/inception_score.py
+++ code/inception_score.py
@@ -130,7 +130,8 @@ def get_inception_score(sess, images, pred_op):
         kl = np.mean(np.sum(kl, 1))
         scores.append(np.exp(kl))
     print('mean:', "%.2f" % np.mean(scores), 'std:', "%.2f" % np.std(scores))
-    printmd('Mean Inception Score : ' + str((np.mean(scores)).item()) )
+    print('Mean Inception Score : ' + str((np.mean(scores)).item()) )
+    printmd('# Mean Inception Score : ' + str((np.mean(scores)).item()) )
     return np.mean(scores), np.std(scores)
 
 
