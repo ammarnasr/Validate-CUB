ccf1ffee60bc4bc422ef9d41f73ac3f94e14e86e
ammarnasr <ammarnasraza@gmail.com>
2020-06-19 20:26:39 +0200

    Update trainer.py

--- code/trainer.py
+++ code/trainer.py
@@ -384,6 +384,7 @@ class condGANTrainer(object):
             # the path to save generated images
             s_tmp = model_dir[:model_dir.rfind('.pth')]
             save_dir = '%s/%s' % (s_tmp, split_dir)
+            print('save_dir:' , save_dir)
             mkdir_p(save_dir)
 
             cnt = 0
